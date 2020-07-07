d5cea2e8d601164a3aa5aa47d15f94c0d36cb623
kevindai777 <kevin.dai.2451@gmail.com>
2020-07-07 09:28:40 -0400

    solutions

---
+++ breakapalindrome.js
@@ -0,0 +1,29 @@
+//Objective is to replace exactly one character in a string to make it not
+//a palindrome, such that the string is as lexicographically low as possible.
+
+let palindrome = "abccba"
+
+
+//O(n) solution that replaces the first non 'a' in the string.
+
+if (palindrome.length <= 1) {
+    return ''
+}
+
+for (let i = 0; i < palindrome.length; i++) {
+    //Skip all 'a' since it's the first character in the alphabet
+    if (palindrome[i] == 'a') {
+        continue
+    }
+    
+    //When the length is odd, the middle character will not change the palindrome
+    if ((palindrome.length % 2 !== 0) && (i == Math.floor(palindrome.length / 2))) {
+        continue
+    }
+    
+    //Replace the first non 'a' in the string with an 'a'
+    return palindrome.substring(0, i) + 'a' + palindrome.substring(i + 1)
+}
+
+//If it's not possible, just add a 'b' at the end
+return palindrome.substring(0, palindrome.length - 1) + 'b'
